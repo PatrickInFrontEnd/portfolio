e9544d3b6375f81c0effc47ef01d9c97eed2cd37
PatrickInFrontEnd <patryk.pluciennik022@gmail.com>
2020-09-01 12:57:46 +0200

    gatsby-browser.js removed - testing SSR

--- gatsby-browser.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import NavigationProvider from "./src/providers/navigation_provider/navigation.provider"
-import ThemeProvider from "./src/providers/theme_provider/theme.provider"
-
-export const wrapRootElement = ({ element }) => (
-  <ThemeProvider>
-    <NavigationProvider>{element}</NavigationProvider>
-  </ThemeProvider>
-)
