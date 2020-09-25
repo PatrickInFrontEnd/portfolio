6526dabdd17d225160f263545442b1b2c209548b
PatrickInFrontEnd <patryk.pluciennik022@gmail.com>
2020-09-25 20:52:09 +0200

    Updated paragraphs data for About section.

--- src/components/sections/About  section/paragraphsData.js
+++ src/components/sections/About  section/paragraphsData.js
@@ -1,8 +1,8 @@
 export const data = [
   "My name is Patrick. I am interested in frontend development since 2017.",
-  "I have recently graduated from school as a Computer specialist with a very good degree.",
+  "I learned all the skills by myself using various learning platforms like Udemy or Eduweb.",
   "Currently I try to gain more and more knowledge about web frameworks like React or Gatsby.",
+  "I have recently graduated from school as a Computer specialist with a very good degree.",
   "Every day I try to cultivate my skills so that my projects are going to be better and better.",
   "I am a humble person. I try to understand problems of other people so that I can help to solve them.",
-  "I can work in group or alone. I find help of other people very useful as it helps me to keep close to good practises.",
 ]
