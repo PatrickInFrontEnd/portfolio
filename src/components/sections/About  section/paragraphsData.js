0d72924ebc63245c8dffe59848b130200eba9d2e
PatrickInFrontend <patryk.pluciennik022@gmail.com>
2021-08-06 22:24:56 +0200

    Content update - about section changed (frameworks learning).

--- src/components/sections/About  section/paragraphsData.js
+++ src/components/sections/About  section/paragraphsData.js
@@ -1,7 +1,7 @@
 export const data = [
   "My name is Patrick. I am interested in frontend development since 2017.",
   "I learned all the skills by myself using various learning platforms like Udemy or Eduweb.",
-  "Currently I try to gain more and more knowledge about web frameworks like React or Gatsby.",
+  "Currently I try to gain more and more knowledge about web frameworks like NextJS or Gatsby.",
   "I have recently graduated from school as a Computer specialist with a very good degree.",
   "Every day I try to cultivate my skills so that my projects are going to be better and better.",
   "I am a humble person. I try to understand problems of other people so that I can help to solve them.",
