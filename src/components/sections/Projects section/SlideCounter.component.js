72c9c2ae1794d25f762ea9d6ca2e1bee3478e469
PatrickInFrontend <patryk.pluciennik022@gmail.com>
2024-06-02 18:43:50 +0200

    refactor: presentation delegated to the component itself

--- src/components/sections/Projects section/SlideCounter.component.js
+++ src/components/sections/Projects section/SlideCounter.component.js
@@ -1,10 +1,13 @@
 import React from "react"
 import { SliderCounterWrapper, NumberOfSlide } from "./Projects.styles"
 
-const SliderCounterContainer = ({ slideNumber }) => (
+const SliderCounterContainer = ({ slideNumber }) => {
+  const formattedSlideNumber = slideNumber < 10 ? `0${slideNumber}` : slideNumber
+
+  return (
   <SliderCounterWrapper>
-    <NumberOfSlide>{slideNumber}</NumberOfSlide>
+    <NumberOfSlide>{formattedSlideNumber}</NumberOfSlide>
   </SliderCounterWrapper>
-)
+)}
 
 export default SliderCounterContainer
