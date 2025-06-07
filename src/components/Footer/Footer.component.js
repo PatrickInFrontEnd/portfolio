6ffdec849af56502eb38a346eac970ce2f0e3bae
PatrickInFrontend <patryk.pluciennik022@gmail.com>
2025-06-07 11:52:32 +0200

    chore: updated footer component to use current year

--- src/components/Footer/Footer.component.js
+++ src/components/Footer/Footer.component.js
@@ -1,37 +1,44 @@
-import React from "react"
-import {
-  FooterWrapper,
-  FooterTitle,
-  DirectorLink,
-  LicenseRights,
-  BackToTopButtonWrapper,
-  BackToTopButton,
-} from "./Footer.styles"
+import React, { useEffect, useState } from "react"
+import dateLib from "../../utils/date"
 import GithubIcon from "./../../assets/images/icon_footer_github.svg"
 import { scrollToSection } from "./../../utils/scrollToSection"
+import {
+  BackToTopButton,
+  BackToTopButtonWrapper,
+  DirectorLink,
+  FooterTitle,
+  FooterWrapper,
+  LicenseRights,
+} from "./Footer.styles"
 
-const FooterComponent = props => (
-  <FooterWrapper>
-    <FooterTitle>
-      Portfolio project - landing page &copy;2020. All rights reserved.
-    </FooterTitle>
-    <LicenseRights href="./policies/license_rights.txt" download>
-      License rights
-    </LicenseRights>
-    <DirectorLink
-      href="https://github.com/PatrickInFrontEnd"
-      target="_blank"
-      rel="noreferrer"
-    >
-      Site created by Patryk Płuciennik
-      <GithubIcon />
-    </DirectorLink>
+const FooterComponent = props => {
+  const [year, setYear] = useState(dateLib.getCurrentYear)
 
-    <BackToTopButtonWrapper
-      onClick={() => scrollToSection("#introduction", 80)}
-    >
-      <BackToTopButton />
-    </BackToTopButtonWrapper>
-  </FooterWrapper>
-)
+  useEffect(() => {
+    setYear(dateLib.getCurrentYear())
+  }, [])
+
+  return (
+    <FooterWrapper>
+      <FooterTitle>
+        Portfolio project - landing page &copy;{year}. All rights reserved.
+      </FooterTitle>
+      <LicenseRights href="./policies/license_rights.txt" download>
+        License rights
+      </LicenseRights>
+      <DirectorLink
+        href="https://github.com/PatrickInFrontEnd"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Site created by Patryk Płuciennik
+        <GithubIcon />
+      </DirectorLink>
+
+      <BackToTopButtonWrapper onClick={() => scrollToSection("#", 80)}>
+        <BackToTopButton />
+      </BackToTopButtonWrapper>
+    </FooterWrapper>
+  )
+}
 export default FooterComponent
